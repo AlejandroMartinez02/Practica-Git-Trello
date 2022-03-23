a6c1a03d5f21cef5e566f4ca99d3d4b8320a4887
Alejandro Martinez <martinezmorilloalejandro@gmail.com>
2022-03-23 19:25:10 +0100

    Todos los forms

--- Practica Git + Trello/Program.cs
+++ Practica Git + Trello/Program.cs
@@ -17,7 +17,7 @@ namespace Practica_Git___Trello
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new formPrincipal());
         }
     }
 }
