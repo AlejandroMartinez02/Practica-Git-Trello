25ad5b678663b295be0a4f68e9ba632509239e5f
Mario Bellido Jiménez <mariobj1999@gmail.com>
2022-03-24 18:48:40 +0100

    Interfaz grafica mejorada del formPrincipal, añadida mecanica responsive.

--- Practica Git + Trello/formPrincipal.Designer.cs
+++ Practica Git + Trello/formPrincipal.Designer.cs
@@ -29,24 +29,130 @@ namespace Practica_Git___Trello
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.boton_D = new System.Windows.Forms.Button();
+            this.boton_C = new System.Windows.Forms.Button();
+            this.boton_B = new System.Windows.Forms.Button();
+            this.boton_A = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // boton_D
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.boton_D.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.boton_D.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.boton_D.FlatAppearance.BorderSize = 0;
+            this.boton_D.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.boton_D.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.boton_D.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.boton_D.Location = new System.Drawing.Point(334, 74);
+            this.boton_D.Margin = new System.Windows.Forms.Padding(0);
+            this.boton_D.Name = "boton_D";
+            this.boton_D.Size = new System.Drawing.Size(334, 74);
+            this.boton_D.TabIndex = 3;
+            this.boton_D.Text = "D";
+            this.boton_D.UseVisualStyleBackColor = false;
+            // 
+            // boton_C
+            // 
+            this.boton_C.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.boton_C.BackColor = System.Drawing.Color.Yellow;
+            this.boton_C.FlatAppearance.BorderSize = 0;
+            this.boton_C.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.boton_C.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.boton_C.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.boton_C.Location = new System.Drawing.Point(0, 74);
+            this.boton_C.Margin = new System.Windows.Forms.Padding(0);
+            this.boton_C.Name = "boton_C";
+            this.boton_C.Size = new System.Drawing.Size(334, 74);
+            this.boton_C.TabIndex = 2;
+            this.boton_C.Text = "C";
+            this.boton_C.UseVisualStyleBackColor = false;
+            // 
+            // boton_B
+            // 
+            this.boton_B.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.boton_B.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.boton_B.FlatAppearance.BorderSize = 0;
+            this.boton_B.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.boton_B.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.boton_B.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.boton_B.Location = new System.Drawing.Point(334, 0);
+            this.boton_B.Margin = new System.Windows.Forms.Padding(0);
+            this.boton_B.Name = "boton_B";
+            this.boton_B.Size = new System.Drawing.Size(334, 74);
+            this.boton_B.TabIndex = 1;
+            this.boton_B.Text = "B";
+            this.boton_B.UseVisualStyleBackColor = false;
+            // 
+            // boton_A
+            // 
+            this.boton_A.BackColor = System.Drawing.Color.Brown;
+            this.boton_A.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.boton_A.FlatAppearance.BorderSize = 0;
+            this.boton_A.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.boton_A.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.boton_A.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.boton_A.Location = new System.Drawing.Point(0, 0);
+            this.boton_A.Margin = new System.Windows.Forms.Padding(0);
+            this.boton_A.Name = "boton_A";
+            this.boton_A.Size = new System.Drawing.Size(334, 74);
+            this.boton_A.TabIndex = 0;
+            this.boton_A.Text = "A";
+            this.boton_A.UseVisualStyleBackColor = false;
+            this.boton_A.Click += new System.EventHandler(this.boton_A_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.boton_A);
+            this.panel1.Controls.Add(this.boton_C);
+            this.panel1.Controls.Add(this.boton_B);
+            this.panel1.Controls.Add(this.boton_D);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.panel1.Location = new System.Drawing.Point(0, 245);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 157);
+            this.panel1.TabIndex = 5;
+            this.panel1.SizeChanged += new System.EventHandler(this.CambioTamaño);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(700, 68);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1 ¿Cual es la velocidad de la luz?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // formPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(700, 402);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "formPrincipal";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.formPrincipal_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button boton_D;
+        private System.Windows.Forms.Button boton_C;
+        private System.Windows.Forms.Button boton_B;
+        private System.Windows.Forms.Button boton_A;
+        private System.Windows.Forms.FlowLayoutPanel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Practica Git + Trello/formPrincipal.cs
+++ Practica Git + Trello/formPrincipal.cs
@@ -7,15 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Practica_Git___Trello
 {
     public partial class formPrincipal : Form
     {
+
+
         public formPrincipal()
         {
             InitializeComponent();
+            boton_A.BackColor = ColorTranslator.FromHtml("#e21b3c");
+            boton_B.BackColor = ColorTranslator.FromHtml("#1268cd");
+            boton_C.BackColor = ColorTranslator.FromHtml("#d89e00");
+            boton_D.BackColor = ColorTranslator.FromHtml("#2a8f0d");
+
+            Stopwatch timer = new Stopwatch();
+
+
         }
 
+        private void boton_A_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CambioTamaño(object sender, EventArgs e)
+        {
+            boton_A.Size = new Size(panel1.Size.Width / 2, panel1.Size.Height / 2);
+            boton_B.Size = new Size(panel1.Size.Width / 2, panel1.Size.Height / 2);
+            boton_C.Size = new Size(panel1.Size.Width / 2, panel1.Size.Height / 2);
+            boton_D.Size = new Size(panel1.Size.Width / 2, panel1.Size.Height / 2);
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cambiarPanel(object sender, EventArgs e)
+        {
+            formPrincipal f1 = new formPrincipal();
+            panel1.Size = new Size(f1.Size.Width, f1.Size.Height / 3);
+        }
     }
 }
