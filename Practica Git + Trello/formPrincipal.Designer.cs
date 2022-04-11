e26955dbbba18a927b692e6c893ce4a5446f2b3a
DavidFerrerBarco <pizca777@gmail.com>
2022-04-11 19:35:30 +0200

    Ajuste del form principal

--- Practica Git + Trello/formPrincipal.Designer.cs
+++ Practica Git + Trello/formPrincipal.Designer.cs
@@ -35,10 +35,9 @@ namespace Practica_Git___Trello
             this.boton_B = new System.Windows.Forms.Button();
             this.boton_A = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.lbl_pregunta = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
@@ -47,7 +46,7 @@ namespace Practica_Git___Trello
             // boton_D
             // 
             this.boton_D.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.boton_D.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(167)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.boton_D.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(123)))), ((int)(((byte)(166)))));
             this.boton_D.FlatAppearance.BorderSize = 0;
             this.boton_D.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.boton_D.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
@@ -64,7 +63,7 @@ namespace Practica_Git___Trello
             // boton_C
             // 
             this.boton_C.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.boton_C.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(119)))), ((int)(((byte)(187)))));
+            this.boton_C.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(186)))), ((int)(((byte)(150)))));
             this.boton_C.FlatAppearance.BorderSize = 0;
             this.boton_C.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.boton_C.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
@@ -81,7 +80,7 @@ namespace Practica_Git___Trello
             // boton_B
             // 
             this.boton_B.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.boton_B.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(169)))), ((int)(((byte)(107)))));
+            this.boton_B.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(193)))), ((int)(((byte)(217)))));
             this.boton_B.FlatAppearance.BorderSize = 0;
             this.boton_B.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.boton_B.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
@@ -97,7 +96,7 @@ namespace Practica_Git___Trello
             // 
             // boton_A
             // 
-            this.boton_A.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(223)))), ((int)(((byte)(127)))));
+            this.boton_A.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(158)))), ((int)(((byte)(76)))));
             this.boton_A.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.boton_A.FlatAppearance.BorderSize = 0;
             this.boton_A.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -110,7 +109,7 @@ namespace Practica_Git___Trello
             this.boton_A.TabIndex = 0;
             this.boton_A.Text = "A";
             this.boton_A.UseVisualStyleBackColor = false;
-            this.boton_A.Click += new System.EventHandler(this.clickBotonA);
+            this.boton_A.Click += new System.EventHandler(this.responder);
             // 
             // panel1
             // 
@@ -127,45 +126,56 @@ namespace Practica_Git___Trello
             this.panel1.Size = new System.Drawing.Size(800, 209);
             this.panel1.TabIndex = 5;
             // 
-            // lbl_pregunta
-            // 
-            this.lbl_pregunta.Location = new System.Drawing.Point(0, 0);
-            this.lbl_pregunta.Name = "lbl_pregunta";
-            this.lbl_pregunta.Size = new System.Drawing.Size(100, 23);
-            this.lbl_pregunta.TabIndex = 0;
-            // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(75, 48);
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(247)))), ((int)(((byte)(193)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.Size = new System.Drawing.Size(800, 91);
             this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
+            this.label1.Text = "1 ¿Cual es la velocidad de la luz?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.contador);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.contador2);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(257, 89);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.Size = new System.Drawing.Size(800, 25);
             this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
+            this.label2.Text = "Pregunta 1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(104, 158);
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Microsoft PhagsPa", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(702, 76);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.Size = new System.Drawing.Size(62, 52);
             this.label3.TabIndex = 8;
-            this.label3.Text = "label3";
+            this.label3.Text = "30";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // formPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(247)))), ((int)(((byte)(193)))));
             this.ClientSize = new System.Drawing.Size(800, 536);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -177,7 +187,6 @@ namespace Practica_Git___Trello
             this.SizeChanged += new System.EventHandler(this.cambiarTamanyo);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -188,10 +197,9 @@ namespace Practica_Git___Trello
         private System.Windows.Forms.Button boton_B;
         private System.Windows.Forms.Button boton_A;
         private System.Windows.Forms.FlowLayoutPanel panel1;
-        private System.Windows.Forms.Label lbl_pregunta;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
     }
